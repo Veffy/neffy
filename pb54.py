be2aa900c073c4e9571c2843d6e51cf87677b05c
Veffy <47236133+Veffy@users.noreply.github.com>
2019-03-24 22:34:04 +0530

    Create pb54.py

---
+++ pb54.py
@@ -0,0 +1,11 @@
+from numpy import *
+x = range(int(input("x")))
+x = reshape(x,(4,4))
+print(x) 
+a = int(input("enter row limits"))
+b = int(input("enter col limits"))
+c = [0] * a
+for i in range(n):
+    c[i] = [0] * b
+    print("  ")
+print(c)

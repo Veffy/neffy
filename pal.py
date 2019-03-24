5d1ee68d5314b25491623e225c9b5ce42a59e332
Veffy <47236133+Veffy@users.noreply.github.com>
2019-03-24 20:55:42 +0530

    Create pal.py

---
+++ pal.py
@@ -0,0 +1,7 @@
+num = input('Enter any number : ')
+try:
+    val = int(num)
+    if num == str(num)[::-1]:
+        print('The given number is PALINDROME')
+    else:
+        print('The given number is NOT a palindrome')

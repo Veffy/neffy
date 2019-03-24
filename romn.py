c28649007d089562ebdf4a5273d2b369a768b006
Veffy <47236133+Veffy@users.noreply.github.com>
2019-03-24 21:45:27 +0530

    Update and rename rom. py to romn.py

--- romn.py
+++ romn.py
@@ -1,4 +1,4 @@
-def romn_to_int(string):
+def rom_to_int(string):
     table=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]]
     returnint=0
     for pair in table:
@@ -12,5 +12,5 @@ def romn_to_int(string):
             else: continueyes=False
 
     return returnint
-romn=input()
-print(romn_to_int(romn))
+rom = input()
+print(rom_to_int(rom))
